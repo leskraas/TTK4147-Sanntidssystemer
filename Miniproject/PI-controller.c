2b377f49a365721c7dbcc8f05428674cdca3f0b4
Lars Erik Skraastad <larserikskraastad@dhcp-10-22-78-219.wlan.ntnu.no>
2017-10-29 19:57:00 +0100

    GET_ACK string break success

--- Miniproject/PI-controller.c
+++ Miniproject/PI-controller.c
@@ -7,38 +7,46 @@ static char ip[] = "127.0.0.1";
 
 int main(void)
 {
+	//----------------------
 	struct udp_conn udp;
-
-
 	udp_init_client(&udp, port, ip);
 
+
+	//-----------START-----------
 	int len = 6;
 	char buf[6] = "START";
 
 	udp_send(&udp, buf, len);
 
-	len = 4;
-	//buf[4] = "GET";
-	
-	udp_send(&udp, "GET", len);
 
+
+	//-----------GET-----------
+	udp_send(&udp, "GET", 4);
+
+
+	//-----------GET_ACK-----------
 	char msg_receive[100];
-	//printf("er her1\n");
-
-
+	char *found;
 	udp_receive(&udp, msg_receive, 100);
+	
+	found = strtok(msg_receive, ":");
+	while(found != NULL){
+		found = strtok(NULL, ":");
+	}
+	
 
-	unsigned int  i;
-	char *temp;
-	printf("%lu\n", strlen(msg_receive));
-	for (i = 0; i < strlen(msg_receive)-8; i++){
-		printf("dette går bra\n");
-		printf("%\n", &temp[0]);  
-	} 
-	printf("received msg: %s\n", temp);
-	printf("%s\n", msg_receive);
+	
+        //printf("%s\n",found);
 
 
+	//-----------SET-----------
+	char set[100];
+	strcpy(set, "SET:");
+	strcat(set, found); 
+	udp_send(&udp, set, 100);
+
+
+	//-----------STOP-----------
 	udp_send(&udp, "STOP", 5);
 
 	udp_close(&udp);
